d05bd6f21d09ce7fd356b024a089464107b9e90e
Rudy Harun Arrasyid <57350097+RudyHarunArrasyid@users.noreply.github.com>
2019-11-22 13:40:41 +0700

    Add files via upload

--- digimon.py
+++ digimon.py
@@ -49,6 +49,7 @@ for i in range(len(listDigi)):
     listDigi[i].insert(0,gambar[i])
     listDigi[i].insert(0,num[i])
 # print(listDigi)
+
 digimon=[]
 for i in listDigi:
     dictDigi=dict(zip(head,i))
@@ -88,3 +89,27 @@ for a,b,c,d,e,f,g,h,i,j,k,l,m,n in listDigi:
     sheet.write(row,13,n)
     row+=1
 file.close()
+
+tupleDigi=[]
+for i in listDigi:
+    tupleDigi.append(tuple(i))
+# print(tupleDigi)
+
+import mysql.connector
+db=mysql.connector.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='rudeboy99',
+    database='digimon',
+)
+
+# print(db)
+c=db.cursor()
+# c.execute('create database digimon')
+# tabel='create table digi(No int,Picture varchar(100),Digimon varchar(20),Stage varchar(20),Type varchar(20),Attribute varchar(20),Memory int,Equip_Slots int,HP int,SP int,Atk int,Def int,Intel int,Spd int)'
+# c.execute(tabel)
+sql='insert into digi (No,Picture,Digimon,Stage,Type,Attribute,Memory,Equip_Slots,HP,SP,Atk,Def,Intel,Spd) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+val=tupleDigi
+c.executemany(sql,val)
+db.commit()
